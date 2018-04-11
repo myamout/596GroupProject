8aef9168b6012c5d82236aa222dcbc44f039814a
Matthew Yamout <matthewyamout@Matthews-MacBook-Pro.local>
2018-04-11 15:16:20 -0700

    meh

--- main.py
+++ main.py
@@ -11,14 +11,38 @@ def main():
 
   # grab all of the players
   cursor.execute("SELECT * FROM Player")
-  players_dataframe = pd.DataFrame(cursor.fetchall(), 
+  players = pd.DataFrame(cursor.fetchall(), 
     columns=["id", "player_api_id", "player_name", "player_fifa_api_id", "birthday", "height", "weight"])
 
   # grab all of the players' attributes
   cursor.execute("SELECT * FROM Player_Attributes")
-  players_attributes_dataframe = pd.DataFrame(cursor.fetchall(), 
+  players_attributes = pd.DataFrame(cursor.fetchall(), 
     columns=["id", "player_fifa_api_id", "player_api_id", "date", "overall_rating", "potential", "preferred_foot", "attacking_work_rate", "defensive_work_rate", "crossing", "finishing", "heading_accuracy", "short_passing", "volleys", "dribbling", "curve", "free_kick_accuracy", "long_passing", "ball_control", "acceleration", "sprint_speed", "agility", "reactions", "balance", "shot_power", "jumping", "stamina", "strength", "long_shots", "aggression", "interceptions", "positioning", "vision", "penalties", "marking", "standing_tackle", "sliding_tackle", "gk_diving", "gk_handling", "gk_kicking", "gk_positioning", "gk_reflexes"])
   
+  # I'm not sure we even need the players_dataframe
+  # we'll see
+
+  # replace attacking work rate and defensive work rate with numbers
+  # 1 = low, 2 = medium, 3 = high
+  # These steps are kinda repeatative, we can fix this
+  players_attributes.attacking_work_rate.replace(["low", "medium", "high"], [1, 2, 3], inplace=True)
+  players_attributes.defensive_work_rate.replace(["low", "medium", "high"], [1, 2, 3], inplace=True)
+
+  # Labels (overall_rating)
+  labels = players_attributes["overall_rating"].values
+  
+  # Drop shit we don't need
+  players_attributes.drop("overall_rating", axis=1)
+  players_attributes.drop("id", axis=1)
+  players_attributes.drop("player_fifa_api_id", axis=1)
+  players_attributes.drop("player_api_id", axis=1)
+  players_attributes.drop("date", axis=1)
+  players_attributes.drop("potential", axis=1)
+  players_attributes.drop("preferred_foot", axis=1)
+  
+  # Create our feature data
+  print(players_attributes[:5])
+
 
 if __name__ == '__main__':
   main()
